009217db0cce2c978b947012992f4456218863fb
Liran Tal <liran.tal@gmail.com>
2019-11-11 00:25:01 +0200

    feat(avatars): add analyst avatar

---
+++ src/AnalystsAvatars.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const avatars = [
+  `
+'''''''''''''''          -so::-+ydmmdmmNNNNNNmmmmmmdddh+-'     '''''''''''''''''
+'''''''''''''''''    '''.s/-+ydNMMMMMNNNNNmmNNNNNNNNNmmmdh+'    ''''''''''''''''
+'''''''''''''''''  ''.-.+yysdNNMMMMMMMMMMMMMNMMMMMNNNNNNNNdd:  '''''''''''''''''
+''''''''''''''''   '.oy+shmNNNMMMMMMMMMMMNNNNMMMMMMNMMMNNMNNN:  '.''''''''''''''
+'''''''''''''''    '-+hddNNNMMMMMMMNMMMMMNNMMMMMMMMNNMMMMNNMMN- '..'''''''''''''
+''''''''''''''''  'yssdmmNNNNNNMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMd''..'''''''''''''
+''''''''''''''''' '+dmmmNmNNNNNNNNMMMMMMMMNNNMMNMMMMNMMMMMMMMMN-'.''''''''''''''
+'''''''''''''''''' '.:/hddmmmddNNNNNMMMMMMNNNMMNNNMMMMMMMMMMMMM:'.''''''''''''''
+'''''''''''''''''''    '/hhhhhhhdmmmNNNNNNNNNNMNNNNNNNMMMMMMMMM:''''''''''''''''
+'''''''''''''''''''''   /hhhhyysssyyhhhhdmNNNmmdmNMMMMNMMMMMMMm'''''''''''''''''
+'''''''''''''''''''''' :NNMNmhyyhhddddhhsoosssooshNMMMMMMMMMMMh''.''''''''''''''
+''''''''''''''''''''''''oNMMMNyyhdNMMMNNNmdysooo+odMMMMMMMMMMMs'..''''''''''''''
+''''''''''''''''''''''' 'ymMNy++shmNNNMmdNNdys++++dMMMMMNdhhmm.'..''''''''''''''
+''''''''''''''''''''''''.ohds///+oshdmdhyssso++//+hMMNdhsyoyy:'...''''''''''''''
+'''''''''''''''''''''''':ss++////+ooossoo++++++/++hNdooydhyss''..'''''''''''..-/
+'''''''''''''''''''''''':osoo+///+sso+ooooo++++ooymmso+yyhhy:''..''''''''''''.-:
+''''''''''''''''''''''''oshddhmmhyssso++++oooosshmNmyssooss:'''..''''''''''''''.
+''''''''''''''''''''''':hhNMMMMMMmdysso++++oosydNNNmhsoo+:.'''.--.''''''''''''.-
+-.---.-..-.--........../mMMMNNMMMMNNdysoo+ooyhmmNMNysohs-.....-::-''.----------/
+::::-------..''.''...''/NMMMNNmmNNNNNNmhyyyyhmNMNNNh+sh/////////+:..-+++oooooosy
+'''''    ''''''''''.'''oNMMmdddhhddmNMMNNmmmNNMNNNms+sys++oyhso+oyyysso+syyo+oyd
+'''''''......------::..sNMNNNNNNmmdmNMMMMMMMMMMMNhs+///:--/++/::/oo+++/:/oo+/+oh
+.......----.-----:::.-:/hNNMMMMNNNNMMMMMMMMMMMMmyoooo'::--::::--:+/:-----------:
+.....----:-.-::-:--.-:-'sNMMMMMNNMMMMMMMMMMMMdyssoomd:''''.......:/-............
+--------:--.---:::.-/- '-dMMMMMMMMMMMMMMMMMmyssssoyMm+. '  ''''''.:-''''''''''''
+-:------::-.-:::-../:' './dMMMMMMMMMMMMNmhsosssssymMh+- ''''''  ''-.''''''''''''
+-----:::::-.-::-.-::-'.-:::smMMMMMMMMmmhysssssssydNNy/-'    '.''  ''            
+-----::::-..---.-:::--/sso+-oNNNNmmmdhhyysyssyyyhmNNo:.' ''  '   ''''           
+----::-::-..-..-::----/oo+/.-+dNmmddhhhhhyyyyyyhhmNy/-.'  '''..''''             
+-----:----....-::----::+++/.'.:ddhhhhhhddhyyssyyhms/:-.    '..''''.'''''        
+----------...--:-----:-////-' '+yysssssyyyssssyyhs-::.''   '..''...''..'''''    
+:---------.------------::::::' 'yssooosssssssssyy..--'''   '..''..-''...'''''   
+:--------..-------------:::-::' ossoooosssssssyh:'..''''/.'''-.''.-''...''''''  
+---------...-------------::--:/':yssooosssssyyd/''..'''':/-.'..''.-''...''''''' 
+-----.---...--------------:---::'oyyssyssyyyhdo'  '''''':+-''.....-''...'' '.'''
+-:---..---..-----------.-------:..yyyyyyyyyhdh.   '''.-//:''''...--''..'''..''  
+::--.....--.-----------.----..--''-hhhyyyhhdd:'    '-.--:.''''..-:.'...''...''  
+////:::::-.--.---------..---...-'''oddhhdhdd/''   ':-...-.''''..::''..''...''' '
+///::-::::-------------..----..-..':ddddddm+''' '---....-.'''..-/-''.''.:.'''' '`
+]
+
+module.exports.avatars = avatars
